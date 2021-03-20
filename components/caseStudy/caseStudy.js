7bd71fb185200fbce6245550093d8ce932b18fb2
josemartos <martosjosele@gmail.com>
2021-03-20 21:54:09 +0100

    IO only on images

--- components/caseStudy/caseStudy.js
+++ components/caseStudy/caseStudy.js
@@ -34,36 +34,30 @@ const CaseStudy = () => {
         <div className={cn('flexGrid', styles.titleWrapper)}>
           <div className={cn('col--md-1')}></div>
           <div className={cn('col--md-11')}>
-            <ComeInAnimation position="left">
-              <h1 className={styles.title}>Mirror</h1>
-            </ComeInAnimation>
+            <h1 className={styles.title}>Mirror</h1>
           </div>
         </div>
         <div className={cn('flexGrid', styles.intro)}>
           <div className={cn('col--md-1')}></div>
           <div className={cn('col--md-5')}>
-            <ComeInAnimation position="left">
-              <p>
-                During the first phase at the Designlab’s UX Academy, I had to
-                work on the <strong>branding</strong> and{' '}
-                <strong>design</strong> of a responsive website for a clothing
-                store, with the goal to expand their online presence.
-              </p>
-              <p>
-                I learnt a lot, since I had to dig deeper into clothing
-                e-commerce, conduct customer interviews, create wireframes and
-                UI designs, as well as prototype, test and refine the final
-                iteration.
-              </p>
-            </ComeInAnimation>
+            <p>
+              During the first phase at the Designlab’s UX Academy, I had to
+              work on the <strong>branding</strong> and <strong>design</strong>{' '}
+              of a responsive website for a clothing store, with the goal to
+              expand their online presence.
+            </p>
+            <p>
+              I learnt a lot, since I had to dig deeper into clothing
+              e-commerce, conduct customer interviews, create wireframes and UI
+              designs, as well as prototype, test and refine the final
+              iteration.
+            </p>
           </div>
           <div className={cn('col--md-2')}></div>
           <div className={cn(styles.tagsWrapper, 'col--md-2')}>
-            <ComeInAnimation position="right">
-              <Tag label="Type" text="Course Project - Website" />
-              <Tag label="Role" text="UX / UI Design" />
-              <Tag label="Duration" text="3 Months" />
-            </ComeInAnimation>
+            <Tag label="Type" text="Course Project - Website" />
+            <Tag label="Role" text="UX / UI Design" />
+            <Tag label="Duration" text="3 Months" />
           </div>
         </div>
       </div>
@@ -72,23 +66,19 @@ const CaseStudy = () => {
         <div className={cn('flexGrid', commonStyles.contentWrapper)}>
           <div className={cn('col--md-1')}></div>
           <div className={cn('col--md-8')}>
-            <ComeInAnimation position="left">
-              <h2>Goals</h2>
-            </ComeInAnimation>
-            <ComeInAnimation position="right">
-              <p>
-                Design a logo for the company that is modern and neutral enough
-                to attract all types of people and styles. Create a{' '}
-                <strong>brand</strong> and a <strong>palette color</strong>.
-                Brand message: Neutral, Modern and fresh, Clean and clear.
-              </p>
-              <p>
-                Design a <strong>responsive e-commerce website</strong> that is
-                easy to use and allows customers to browse through all products.
-                Show products for all female, male, kids, casual, business,
-                sport, party, formal, color, size and more.
-              </p>
-            </ComeInAnimation>
+            <h2>Goals</h2>
+            <p>
+              Design a logo for the company that is modern and neutral enough to
+              attract all types of people and styles. Create a{' '}
+              <strong>brand</strong> and a <strong>palette color</strong>. Brand
+              message: Neutral, Modern and fresh, Clean and clear.
+            </p>
+            <p>
+              Design a <strong>responsive e-commerce website</strong> that is
+              easy to use and allows customers to browse through all products.
+              Show products for all female, male, kids, casual, business, sport,
+              party, formal, color, size and more.
+            </p>
           </div>
           <div className={cn('col--md-3')}></div>
         </div>
