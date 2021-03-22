6ed7b948336c74ed7ef8f040f27a51fdedd5322a
josemartos <martosjosele@gmail.com>
2021-03-22 18:44:35 +0100

    Research

--- pages/work/mirror.js
+++ pages/work/mirror.js
@@ -16,8 +16,8 @@ import caseStudy from 'styles/components/caseStudy.module.scss';
 export default function Mirror() {
   return (
     <BasePage title="Mirror e-commerce platform">
-      <section className={pageStyles.section}>
-        <div className={caseStudy.wrapper}>
+      <div className={caseStudy.wrapper}>
+        <section className={pageStyles.section}>
           <header className={caseStudy.hero}>
             <ComeInAnimation>
               <CustomImage
@@ -99,88 +99,232 @@ export default function Mirror() {
               <div className={cn('col--md-3')}></div>
             </div>
           </div>
-        </div>
-      </section>
-      <section className={pageStyles.section}>
-        <CaseStudyBlockHeader title="Understand">
-          <h3>Secondary research</h3>
-          <ul>
-            <li>History of e-commerce</li>
-            <li>Competitor analysis</li>
-            <li>Market and trends report</li>
-          </ul>
-          <h3>Primary research</h3>
-          <ul>
-            <li>User interviews</li>
-            <li>Observation</li>
-          </ul>
-        </CaseStudyBlockHeader>
+        </section>
+        <section className={pageStyles.section}>
+          <CaseStudyBlockHeader title="Understand">
+            <h3>Secondary research</h3>
+            <ul>
+              <li>History of e-commerce</li>
+              <li>Competitor analysis</li>
+              <li>Market and trends report</li>
+            </ul>
+            <h3>Primary research</h3>
+            <ul>
+              <li>User interviews</li>
+              <li>Observation</li>
+            </ul>
+          </CaseStudyBlockHeader>
 
-        <div className={caseStudy.segment}>
-          <h3 className={caseStudy.segmentTitle}>Persona</h3>
-          <div
-            className={cn(
-              'flexGrid',
-              commonStyles.contentWrapper,
-              caseStudy.segmentIntro
-            )}
-          >
-            <div className={cn('col--md-2')}></div>
-            <div className={cn('col--md-8')}>
-              <p>
-                The <strong>Persona creation</strong> helped me to summarize the
-                users’ needs, frustrations and motivations.
-              </p>
+          {/* Research */}
+          <div className={caseStudy.segment}>
+            <h3 className={caseStudy.segmentTitle}>Research</h3>
+            <div
+              className={cn(
+                'flexGrid',
+                commonStyles.contentWrapper,
+                caseStudy.segmentIntro
+              )}
+            >
+              <div className={cn('col--md-2')}></div>
+              <div className={cn('col--md-8')}>
+                <p>
+                  I started doing secondary <strong>research</strong>, by
+                  reading about the
+                  <strong>history and background of e-commerce</strong> to
+                  better understand the scope of the project, conducting{' '}
+                  <strong>competitor analysis</strong> to understand the current
+                  situation, as well as to acknowledge what makes online
+                  shoppers prefer online shopping.
+                </p>
+              </div>
+              <div className={cn('col--md-2')}></div>
             </div>
-            <div className={cn('col--md-2')}></div>
+            <div
+              className={cn(
+                caseStudy.segmentImage,
+                commonStyles.contentWrapper
+              )}
+            >
+              <CustomImage
+                src={'/images/mirror/competitor-analysis.png'}
+                alt="Mirror Competitor Analysis"
+                width={2564}
+                height={708}
+              />
+            </div>
           </div>
-          <div className={commonStyles.contentWrapper}>
-            <CustomImage
-              src={'/images/mirror/persona.jpg'}
-              alt="Mirror Persona"
-              width={2566}
-              height={1682}
-            />
-          </div>
-        </div>
 
-        <div className={caseStudy.segment}>
-          <h3 className={caseStudy.segmentTitle}>Sitemap</h3>
-          <div
-            className={cn(
-              'flexGrid',
-              commonStyles.contentWrapper,
-              caseStudy.segmentIntro
-            )}
-          >
-            <div className={cn('col--md-2')}></div>
-            <div className={cn('col--md-8')}>
-              <p>
-                The <strong>Sitemap</strong> was key to understand what the main
-                structure of the page will be. The page had to have three main
-                categories: Women, Men and Kids. Then the Checkout Funnel as a
-                main point for the next steps.
-              </p>
+          {/* User Interviews */}
+          <div className={caseStudy.segment}>
+            <h3 className={caseStudy.segmentTitle}>User Interviews</h3>
+            <div
+              className={cn(
+                'flexGrid',
+                commonStyles.contentWrapper,
+                caseStudy.segmentIntro
+              )}
+            >
+              <div className={cn('col--md-2')}></div>
+              <div className={cn('col--md-8')}>
+                <p>
+                  I went on to <strong>interview three users</strong> to
+                  understand their experience, what the find pleasant and what
+                  frustrating, to translate that later on in the design. The
+                  following are the findings.
+                </p>
+                <ul>
+                  <li>
+                    <strong>Size</strong> finding is tricky, this needs to be
+                    clearly defined on the page
+                  </li>
+                  <li>
+                    Related to the first point, <strong>returns</strong> tend to
+                    be considered a hassle
+                  </li>
+                  <li>
+                    There are too many <strong>choices</strong>, hence quality
+                    over quantity is preferred
+                  </li>
+                  <li>
+                    The physical <strong>stores</strong> experience is still
+                    very present in the consumer’s needs
+                  </li>
+                  <li>
+                    Payment <strong>security</strong> can still be a concern
+                  </li>
+                  <li>
+                    <strong>Reviews</strong> are an important source of trust
+                  </li>
+                </ul>
+              </div>
+              <div className={cn('col--md-2')}></div>
             </div>
-            <div className={cn('col--md-2')}></div>
           </div>
-          <div className={commonStyles.contentWrapper}>
-            <CustomImage
-              src={'/images/mirror/sitemap.png'}
-              alt="Mirror Sitemap"
-              width={2560}
-              height={1110}
-            />
+        </section>
+        <section className={pageStyles.section}>
+          <CaseStudyBlockHeader title="Define">
+            <h3>Persona</h3>
+            <h3>Information architecture</h3>
+            <ul>
+              <li>Sitemap</li>
+            </ul>
+            <h3>Interaction Design</h3>
+            <ul>
+              <li>Task and User Flow</li>
+              <li>Wireframes</li>
+            </ul>
+          </CaseStudyBlockHeader>
+
+          {/* Persona */}
+          <div className={caseStudy.segment}>
+            <h3 className={caseStudy.segmentTitle}>Persona</h3>
+            <div
+              className={cn(
+                'flexGrid',
+                commonStyles.contentWrapper,
+                caseStudy.segmentIntro
+              )}
+            >
+              <div className={cn('col--md-2')}></div>
+              <div className={cn('col--md-8')}>
+                <p>
+                  The <strong>Persona creation</strong> helped me to summarize
+                  the users’ needs, frustrations and motivations.
+                </p>
+              </div>
+              <div className={cn('col--md-2')}></div>
+            </div>
+            <div
+              className={cn(
+                caseStudy.segmentImage,
+                commonStyles.contentWrapper
+              )}
+            >
+              <CustomImage
+                src={'/images/mirror/persona.jpg'}
+                alt="Mirror Persona"
+                width={2566}
+                height={1682}
+              />
+            </div>
           </div>
-        </div>
-      </section>
-      <section className={pageStyles.section}>
-        <div className={cn(caseStudy.comingSoon, commonStyles.contentWrapper)}>
-          <p>
-            The full info about this project is <strong>coming up soon</strong>
-          </p>
-        </div>
-      </section>
+
+          {/* Sitemap */}
+          <div className={caseStudy.segment}>
+            <h3 className={caseStudy.segmentTitle}>Sitemap</h3>
+            <div
+              className={cn(
+                'flexGrid',
+                commonStyles.contentWrapper,
+                caseStudy.segmentIntro
+              )}
+            >
+              <div className={cn('col--md-2')}></div>
+              <div className={cn('col--md-8')}>
+                <p>
+                  The <strong>Sitemap</strong> was key to understand what the
+                  main structure of the page will be. The page had to have three
+                  main categories: Women, Men and Kids. Then the Checkout Funnel
+                  as a main point for the next steps.
+                </p>
+              </div>
+              <div className={cn('col--md-2')}></div>
+            </div>
+            <div
+              className={cn(
+                caseStudy.segmentImage,
+                commonStyles.contentWrapper
+              )}
+            >
+              <CustomImage
+                src={'/images/mirror/sitemap.png'}
+                alt="Mirror Sitemap"
+                width={2560}
+                height={1110}
+              />
+            </div>
+          </div>
+
+          {/* User Flow */}
+          <div className={caseStudy.segment}>
+            <h3 className={caseStudy.segmentTitle}>User Flow</h3>
+            <div className={cn('flexGrid', commonStyles.contentWrapper)}>
+              <div className={cn('col--md-8', caseStudy.userFlowImage)}>
+                <CustomImage
+                  src={'/images/mirror/user-flow.png'}
+                  alt="Mirror User Flow"
+                  width={1694}
+                  height={2722}
+                />
+              </div>
+              <div className={cn('col--md-4', caseStudy.segmentIntro)}>
+                <p>
+                  The <strong>User Flow</strong> helped me to visualize what the
+                  happy path of a product purchase would be, from the user
+                  landing on the home page to the confirmation page, passing
+                  through the checkout funnel.
+                </p>
+                <p>
+                  Lorem fistrum amatomaa mamaar sexuarl ahorarr por la gloria de
+                  mi madre la caidita benemeritaar. A wan te voy a borrar el
+                  cerito ese que llega va usté muy cargadoo condemor sexuarl
+                  papaar papaar no puedor.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={pageStyles.section}>
+          <div
+            className={cn(caseStudy.comingSoon, commonStyles.contentWrapper)}
+          >
+            <p>
+              The full info about this project is{' '}
+              <strong>coming up soon</strong>
+            </p>
+          </div>
+        </section>
+      </div>
     </BasePage>
   );
 }
