12e7cb841026b42a7b554259aaad526714844509
josemartos <martosjosele@gmail.com>
2024-09-17 09:43:39 +0200

    Update countries

--- pages/about.js
+++ pages/about.js
@@ -60,10 +60,10 @@ export default function About() {
                 Asia, and I am delighted to say that have visited a total of{' '}
                 <a
                   target="_blank"
-                  href="https://www.mytravelmap.xyz/u/gg108230143559826306104"
+                  href="https://visitedplaces.com/view/?map=world&projection=geoOrthographic&theme=dark-green&water=1&graticule=0&names=1&duration=2000&placeduration=100&slider=0&autoplay=1&autozoom=none&autostep=1&home=DE&places=My%20Home~DE~1_0_0_-10.4_-51*Europe~ES_DE.daaa44_GB_IE_FR_AT_NL_EE_SE_CZ_PL_BA_HR_IT_GR_NO_HU_MT_VA_ME_DK~2.4_12.1_53.3_-12.1_-53.3*Africa~MA~1.5_18_1.6_-18_-1.6*Asia~TH_KH_NP_MM_VN_JP_CN_IN~1.5_83.8_33.3_-83.8_-33.3*North%20America~US~1.6_-100.6_44.4_100.6_-44.4"
                   rel="noreferrer"
                 >
-                  31 countries
+                  32 countries
                 </a>{' '}
                 so far. Apart from my hometown, Antequera, I have also lived in
                 Málaga, Madrid and Berlin.
