ed9c8d1f1885f3abff9a37e517733c6e14a5cffb
josemartos <martosjosele@gmail.com>
2024-12-05 18:18:19 +0100

    Bio tweaks

--- pages/about.js
+++ pages/about.js
@@ -45,19 +45,17 @@ export default function About() {
                 .
               </p>
               <p>
-                After more than 11 years as a <strong>developer</strong> on
-                various projects, I decided in 2020 to expand my skill set to
-                include UX/UI design. This shift allowed me to combine my
-                programming expertise with a passion for researching, designing,
-                and prototyping innovative, human-centered interfaces. This
-                combination of skills allows me to oversee the entire product
-                lifecycle, from concept to delivery.
+                After spending over 11 years as a developer on various projects,
+                I expanded my skill set in 2020 to include UX/UI design,
+                blending technical expertise with a passion for crafting
+                user-centered digital solutions.
               </p>
               <p>
                 In my free time, I enjoy reading, hiking, cooking, biking,
-                exploring, photography, and practicing martial arts. In 2018 and
-                2019, I embarked on a four-month solo backpacking adventure
-                across Asia. I'm proud to say I've visited a total of{' '}
+                exploring, photography, and practicing martial arts. Between
+                2018 and 2019, I embarked on a four-month solo backpacking
+                adventure across Asia, and I'm proud to say I have visited a
+                total of{' '}
                 <a
                   target="_blank"
                   href="https://visitedplaces.com/view/?map=world&projection=geoOrthographic&theme=dark-green&water=1&graticule=0&names=1&duration=2000&placeduration=100&slider=0&autoplay=1&autozoom=none&autostep=1&home=DE&places=My%20Home~DE~1_0_0_-10.4_-51*Europe~ES_DE.daaa44_GB_IE_FR_AT_NL_EE_SE_CZ_PL_BA_HR_IT_GR_NO_HU_MT_VA_ME_DK~2.4_12.1_53.3_-12.1~-53.3*Africa~MA~1.5_18_1.6_-18~-1.6*Asia~TH_KH_NP_MM_VN_JP_CN_IN~1.5_83.8_33.3~-83.8~-33.3*North%20America~US~1.6_-100.6_44.4~100.6~-44.4"
@@ -65,12 +63,13 @@ export default function About() {
                 >
                   32 countries
                 </a>{' '}
-                so far. Besides my hometown of Antequera, I've also lived in
-                Málaga, Madrid, and Berlin.
+                so far. Beyond my hometown of Antequera, I've experienced life
+                in Málaga, Madrid, and Berlin, each leaving its mark on my
+                journey.
               </p>
               <p>
-                Since March 2023, I've been a proud father to a curious,
-                energetic, and always-smiling little one named Mila &hearts;
+                Since March 2023, I've been the proud father of a curious,
+                energetic, and always-smiling little one named Mila &hearts;.
               </p>
               <p>
                 Feel free to check out my{' '}
