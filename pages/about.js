dda8069beb79c941d2eae71b68c533649c024e8c
josemartos <martosjosele@gmail.com>
2024-12-29 12:13:13 +0100

    CV small tweaks

--- pages/about.js
+++ pages/about.js
@@ -51,11 +51,10 @@ export default function About() {
                 user-centered digital solutions.
               </p>
               <p>
-                In my free time, I enjoy reading, hiking, cooking, biking,
-                exploring, photography, and practicing martial arts. Between
-                2018 and 2019, I embarked on a four-month solo backpacking
-                adventure across Asia, and I'm proud to say I have visited a
-                total of{' '}
+                In my free time, I enjoy reading, hiking, biking, exploring,
+                photography, and practicing martial arts. Between 2018 and 2019,
+                I embarked on a four-month solo backpacking adventure across
+                Asia, and I'm proud to say I have visited a total of{' '}
                 <a
                   target="_blank"
                   href="https://visitedplaces.com/view/?map=world&projection=geoOrthographic&theme=dark-green&water=1&graticule=0&names=1&duration=2000&placeduration=100&slider=0&autoplay=1&autozoom=none&autostep=1&home=DE&places=My%20Home~DE~1_0_0_-10.4_-51*Europe~ES_DE.daaa44_GB_IE_FR_AT_NL_EE_SE_CZ_PL_BA_HR_IT_GR_NO_HU_MT_VA_ME_DK~2.4_12.1_53.3_-12.1~-53.3*Africa~MA~1.5_18_1.6_-18~-1.6*Asia~TH_KH_NP_MM_VN_JP_CN_IN~1.5_83.8_33.3~-83.8~-33.3*North%20America~US~1.6_-100.6_44.4~100.6~-44.4"
